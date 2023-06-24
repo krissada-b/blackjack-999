bc980732a83217b3785144684a0e268b745fb2cb
arm-kittichot <kittichot.kp@gmail.com>
2023-06-24 11:35:43 +0700

    w

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,7 +8,7 @@ const Home = () => {
     <div className="w-screen h-screen flex items-center justify-center flex-col">
       <div className='w-auto'>
 				<div className='flex flex-col items-center'>
-					<p className='text-6xl my-5'>Blackjack 499</p>
+					<p className='text-6xl my-5'>Blackjack 999</p>
 
 					<Link className='animate-bounce' to={'/game'}>
 						<img style={{width:"100px"}} src={playBtn} alt="" /></Link>
